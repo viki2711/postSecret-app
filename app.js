6b91413f4607effe8f147a09029f2637c01b6c43
Vicky BRS <viki2711@gmail.com>
2021-04-02 16:09:26 -0400

    Add environment vars

---
+++ app.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+// Mongoose encryption
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+const User = mongoose.model('User', userSchema);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", function(req, res){
+  User.findOne({email: req.body.username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser && foundUser.password === req.body.password) {
+        res.render("secrets");
+      } else {
+        res.send("Wrong user!.");
+      }
+    }
+  });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+  console.log("Server is running successfully.");
+});
